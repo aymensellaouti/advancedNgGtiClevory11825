74c03b607c47f1887ea6c1d2b19f66d44ae41e13
aymensellaouti <aymen.sellaouti@gmail.com>
2025-08-12 12:22:11 +0100

    images api 2

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -3,4 +3,5 @@ export const API = {
   login: 'https://apilb.tridevs.net/api/Users/login',
   products: 'https://dummyjson.com/products',
   images: 'https://picsum.photos/v2/list',
+  images2: 'https://fakestoreapiserver.reactbd.com/photos',
 };
