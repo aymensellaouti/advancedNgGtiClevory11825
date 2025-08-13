10635e5ccb148c7f1521d2c474f1b598b3065a81
aymensellaouti <aymen.sellaouti@gmail.com>
2025-08-13 10:34:48 +0100

    correction Product

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, inject } from '@angular/core';
 import {
   BehaviorSubject,
   Observable,
@@ -6,10 +6,10 @@ import {
   map,
   takeWhile,
   scan,
-} from "rxjs";
-import { Product } from "./dto/product.dto";
-import { ProductService } from "./services/product.service";
-import { Settings } from "./dto/product-settings.dto";
+} from 'rxjs';
+import { Product } from './dto/product.dto';
+import { ProductService } from './services/product.service';
+import { Settings } from './dto/product-settings.dto';
 
 @Component({
   selector: 'app-products',
@@ -17,9 +17,34 @@ import { Settings } from "./dto/product-settings.dto";
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
+  setting: Settings = { limit: 12, skip: 0 };
+  settings$ = new BehaviorSubject<Settings>(this.setting);
+  getMoreButtonDisabled = false;
+  productService = inject(ProductService);
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
-  constructor() {}
-  more() {}
+  products$: Observable<Product[]> = this.settings$.pipe(
+    // {12, 0}, {12, 12}, {12, 24} .....
+    concatMap((settings) => this.productService.getProducts(settings)),
+    // APiResponse1, ApiResponse2, ....
+    map((apiReponse) => apiReponse.products),
+    // products1, products2, ...
+    takeWhile((products) => {
+      if (products.length <= 0) {
+        this.getMoreButtonDisabled = true;
+        return false;
+      } else {
+        return true;
+      }
+    }),
+    scan((oldProducts, newProduts) => [...oldProducts, ...newProduts])
+    // [nlem les produits lokol]
+  );
+  more() {
+    this.setting = {
+      ...this.setting,
+      skip: this.setting.skip + this.setting.limit,
+    };
+    this.settings$.next(this.setting);
+  }
 }
