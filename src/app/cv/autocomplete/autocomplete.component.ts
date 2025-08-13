e2cb939e1059542d94c8d1ab1aa41f91c5e3fcf5
aymensellaouti <aymen.sellaouti@gmail.com>
2025-08-13 10:58:23 +0100

    autocomplete correction

--- src/app/cv/autocomplete/autocomplete.component.ts
+++ src/app/cv/autocomplete/autocomplete.component.ts
@@ -1,18 +1,27 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { FormBuilder, AbstractControl } from "@angular/forms";
-import { debounceTime, distinctUntilChanged, switchMap, tap } from "rxjs";
+import { debounceTime, distinctUntilChanged, Observable, switchMap, tap } from "rxjs";
 import { CvService } from "../services/cv.service";
+import { Cv } from "../model/cv";
 
 @Component({
-  selector: "app-autocomplete",
-  templateUrl: "./autocomplete.component.html",
-  styleUrls: ["./autocomplete.component.css"],
+  selector: 'app-autocomplete',
+  templateUrl: './autocomplete.component.html',
+  styleUrls: ['./autocomplete.component.css'],
 })
-export class AutocompleteComponent {
+export class AutocompleteComponent implements OnInit {
   formBuilder = inject(FormBuilder);
   cvService = inject(CvService);
+  cvs$!: Observable<Cv[]>;
+  form = this.formBuilder.group({ search: [''] });
   get search(): AbstractControl {
-    return this.form.get("search")!;
+    return this.form.get('search')!;
   }
-  form = this.formBuilder.group({ search: [""] });
+  ngOnInit(): void {
+    this.cvs$ = this.search.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap((name) => this.cvService.selectByName(name))
+    );
+  }
 }
