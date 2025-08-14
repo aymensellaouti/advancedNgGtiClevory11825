b8777b3a0945cf69157521ecb9871f88e0c6b0d3
aymensellaouti <aymen.sellaouti@gmail.com>
2025-08-14 12:44:44 +0100

    goToStandaloneApplication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { ApplicationRef, Component, inject } from "@angular/core";
 import { LoggerService } from "./services/logger.service";
 import { LoggersInjectionToken } from "./injection tokens/logger.injection-token";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { RouterOutlet } from "@angular/router";
+import { PereCdComponent } from "./cd/pere-cd/pere-cd.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: false
+    imports: [NavbarComponent, RouterOutlet, PereCdComponent]
 })
 export class AppComponent {
   title = 'Starting Advanced Topics';
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,61 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { AuthInterceptorProvider } from './app/auth/interceptors/auth.interceptor';
+import { LoggersInjectionToken } from './app/injection tokens/logger.injection-token';
+import { LoggerService } from './app/services/logger.service';
+import { Logger2Service } from './app/services/logger2.service';
+import { CvService } from './app/cv/services/cv.service';
+import { CONSTANTES } from 'src/config/const.config';
+import { FakeCvService } from './app/cv/services/fake-cv.service';
+import { UUID } from './app/injection tokens/uuid.injection-token';
+import { v4 as uuidV4 } from 'uuid';
+import { myCustomProvider } from './app/providers/custom.provider';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { AppRoutingModule } from './app/app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { isDevMode, importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule
-//   , {
-//   ngZone: 'noop'
-// }
-)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, FormsModule, // required animations module
+        ToastrModule.forRoot(), // ToastrModule added
+        AppRoutingModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
+        })),
+        AuthInterceptorProvider,
+        {
+            provide: LoggersInjectionToken,
+            useClass: LoggerService,
+            multi: true,
+        },
+        {
+            provide: LoggersInjectionToken,
+            useClass: Logger2Service,
+            multi: true,
+        },
+        {
+            // esm el 7aja eli bech nwafarha
+            provide: CvService,
+            // chneya bech nwafer
+            useClass: CONSTANTES.env == 'dev' ? FakeCvService : CvService,
+        },
+        {
+            provide: UUID,
+            useValue: uuidV4,
+        },
+        myCustomProvider,
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
